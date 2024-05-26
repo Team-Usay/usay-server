6a466f58ddd69814a58f84446ff273db9363e672
Hyoseop Song <crad_on25@naver.com>
2024-05-26 14:24:31 +0900

    feat: 400,404,500 비즈니스 예외 구성

---
+++ src/main/java/usay/app/global/exception/InvalidException.java
@@ -0,0 +1,18 @@
+package usay.app.global.exception;
+
+import usay.app.domain.dto.ErrorCode;
+
+public class InvalidException extends BusinessException {
+
+	public InvalidException(String message, ErrorCode errorCode) {
+		super(message, errorCode);
+	}
+
+	public InvalidException(String message) {
+		super(message, ErrorCode.INVALID);
+	}
+
+	public InvalidException(ErrorCode errorCode) {
+		super(errorCode);
+	}
+}
