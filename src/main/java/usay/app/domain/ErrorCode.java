03ee41e9b8c5073ccf92c8df47bee3959b9be560
Hyoseop Song <crad_on25@naver.com>
2024-06-09 21:54:17 +0900

    Fix: 조회 실패 예외 코드 추가

--- src/main/java/usay/app/domain/ErrorCode.java
+++ src/main/java/usay/app/domain/ErrorCode.java
@@ -15,6 +15,12 @@ public enum ErrorCode {
 	NOT_FOUND("SAY404", "Not Found"),
 	MEMBER_NOT_FOUND("SAY40410", "Member Not Found"),
 	MEMBER_DETAIL_NOT_FOUND("SAY40411", "Some of Member Detail Not Found"),
+	AI_MODEL_NOT_FOUND("SAY40420", "AI Model Not Found"),
+	AI_MODEL_VERSION_NOT_FOUND("SAY40421", "AI Model Version Not Found"),
+	CHAT_NOT_FOUND("SAY40430", "Chat Not Found"),
+	CHAT_DETAIL_NOT_FOUND("SAY40431", "Some of Chat Detail Not Found"),
+	ROOM_NOT_FOUND("SAY40440", "Room Not Found"),
+
 	// 500 Internal Server Error
 	INTERNAL_SERVER_ERROR("SAY500", "Server Error");
 	// 401 Unauthorized
