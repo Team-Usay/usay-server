8be67acbfc3e0c51bcf847bad0a41c5a353efe2d
Hyoseop Song <crad_on25@naver.com>
2024-06-09 21:18:46 +0900

    docs: 대화 기록 조회 API 명세 (#21)

--- src/main/java/usay/app/domain/chat/controller/ChatController.java
+++ src/main/java/usay/app/domain/chat/controller/ChatController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import usay.app.domain.ApiResponse;
 import usay.app.domain.chat.entity.dto.ChatRequestDTO.PostChatRequest;
@@ -19,24 +20,25 @@ import usay.app.domain.chat.service.ChatService;
 @Tag(name = "답변", description = "사용자 답변 관련 API")
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/api/v1/chats")
 public class ChatController {
 
 	private final ChatService chatService;
 
 	@Operation(summary = "대화방 별 전체 채팅 조회", description = "해당 채팅방 내 모든 대화 기록을 조회합니다.")
-	@GetMapping("/room/{roomId}")
+	@GetMapping("/{roomId}")
 	public ApiResponse<List<GetChatResponse>> getChatList(@PathVariable Long roomId) {
 		return ApiResponse.success(chatService.getChatList(roomId));
 	}
 
-	@Operation(summary = "사용자별 답변 조회", description = "해당 사용자의 답변을 조회합니다.")
-	@GetMapping("/feeds/{memberId}")
+	@Operation(summary = "사용자의 전체 채팅 조회", description = "해당 사용자의 답변을 조회합니다.")
+	@GetMapping("/{memberId}")
 	public ApiResponse<List<GetChatResponse>> getChatListByMemberId(@PathVariable Long memberId) {
 		return ApiResponse.success(chatService.getChatListByMemberId(memberId));
 	}
 
 	@Operation(summary = "채팅방 채팅 등록", description = "해당 채팅방에 채팅을 등록합니다.")
-	@PostMapping("/chat/{memberId}")
+	@PostMapping("/{memberId}")
 	public ApiResponse<PostChatResponse> postChat(
 			@PathVariable Long memberId,
 			@RequestBody PostChatRequest postChatRequest) {
